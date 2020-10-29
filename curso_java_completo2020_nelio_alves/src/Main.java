4d51149a82ea82628aaa65a9912e673e88c55426
jcgsr <cabralguerra@gmail.com>
2020-10-29 00:01:32 -0300

    first commit

---
+++ curso_java_completo2020_nelio_alves/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// 28.10.2020
+		
+		System.out.println("Olá, Java!");
+		System.out.println("Agora mais um curso Java com Nélio Alves!");
+
+	}
+
+}
