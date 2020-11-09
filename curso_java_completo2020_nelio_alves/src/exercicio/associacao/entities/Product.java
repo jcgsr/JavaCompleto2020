eb3a5c84b95c48128c17ae6bb672b87513312d98
jcgsr <cabralguerra@gmail.com>
2020-11-09 08:50:32 -0300

    associação exercício

---
+++ curso_java_completo2020_nelio_alves/src/exercicio/associacao/entities/Product.java
@@ -0,0 +1,34 @@
+package exercicio.associacao.entities;
+
+public class Product {
+	// 09.11.2020
+
+	private String name;
+	private Double price;
+
+	public Product() {
+
+	}
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+}
---
+++ curso_java_completo2020_nelio_alves/src/exercicio/associacao/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package exercicio.associacao.entities.enums;
+
+public enum OrderStatus {
+	
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED
+}
